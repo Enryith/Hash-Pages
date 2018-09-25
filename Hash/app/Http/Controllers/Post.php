82252e0a09b6c00d03a398d7d8551f76c460e078
Keyes Anderson <keand17@iastate.edu>
2018-09-24 20:53:57 -0500

    Started paginator

--- Hash/app/Http/Controllers/Post.php
+++ Hash/app/Http/Controllers/Post.php
@@ -1,9 +1,16 @@
 <?php
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Routing\Controller;
 
 class Post extends Controller
 {
+	public function index()
+	{
+		$users = DB::table('users')->paginate();
 
+		return view('post.index')->with(['users' => $users]);
+	}
 }
