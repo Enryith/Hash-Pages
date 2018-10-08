0ed95aa8473ba0da21959b656453b97c75d3b983
Andrew Peters <adpeters@iastate.edu>
2018-10-07 19:50:10 -0500

    Added formatting to profile picture. Also made the username of user display under the profile picture.

--- Hash/resources/views/user/view.blade.php
+++ Hash/resources/views/user/view.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Login')
 @section('content')
 
-	<img src="{{ url('/storage/profile_pictures/' . $user->getPicture()) }}" alt="Profile Picture" title="{{ $user->getName() }}">
+	<img style= "object-fit: cover;border-radius:50%" width = "200" height = "200" src="{{ url('/storage/profile_pictures/' . $user->getPicture()) }}" alt="Profile Picture" title="{{ $user->getName() }}" >
+
+	<div style= "font-size: 55px"> {{$user->getUsername()}} </div>
 
 @endsection
