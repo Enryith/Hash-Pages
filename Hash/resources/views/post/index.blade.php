ff662dc379b25835bd54ac7600df55f9bdd6f775
Keyes Anderson <keand17@iastate.edu>
2018-10-09 07:55:49 -0500

    added post view

--- Hash/resources/views/post/index.blade.php
+++ Hash/resources/views/post/index.blade.php
@@ -4,7 +4,7 @@
 @php /** @var Illuminate\Pagination\LengthAwarePaginator|App\Entities\Post[] $table */ @endphp
 	<div class="container">
 		@foreach($table as $post)
-			<h2>{{ $post->getTitle() }}</h2>
+			<a href="../post/{{$post->getId()}}"><h2>{{ $post->getTitle() }}</h2></a>
 			<em>{{ $post->getLink() }}</em>
 			<div class="text-muted">By: {{ $post->getAuthor()->getUsername() }}</div>
 			<p>{{ $post->getBody() }}</p>
