1c00033ffe875427e8594d147d1c48a09d73f4b5
Keyes Anderson <keand17@iastate.edu>
2018-10-09 08:11:21 -0500

    added post view pt 2

--- Hash/resources/views/post/view.blade.php
+++ Hash/resources/views/post/view.blade.php
@@ -1,18 +1,12 @@
 @extends('theme.base')
 @section('title', 'Login')
 @section('content')
+	<h1>{{$post->getTitle()}}</h1>
 
-	<div class="row">
-		<div class="col-md-auto">
-			<img class="img-thumbnail img-fluid profile" src="{{ url($user->getPicture() ? $user->getPicturePublic() : "/img/default.jpg") }}" alt="Profile Picture" title="{{ $user->getName() }}">
-		</div>
-		<div class="col">
-			<h1>
-				{{$user->getUsername()}}
-				<small class="text-muted">{{$user->getName()}}</small>
-			</h1>
-			<p class="lead">{!! nl2br(e($user->getBio())) !!}</p>
-		</div>
-	</div>
+	@if (filter_var($post->getLink(), FILTER_VALIDATE_URL))
+		<a href="{{$post->getLink()}}">Link</a>
+	@endif
 
+	<div class="text-muted">{{$post->getAuthor()->getUsername()}}</div>
+	<p>{{$post->getBody()}}</p>
 @endsection
