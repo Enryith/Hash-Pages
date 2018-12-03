bdedf8abe981274e9c56ce88f40f0b53689f8556
Keyes Anderson <keand17@iastate.edu>
2018-12-03 17:44:38 -0600

    finished tag view/edit

--- Hash/app/Http/Controllers/Tag.php
+++ Hash/app/Http/Controllers/Tag.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Repositories\Posts;
 use App\Repositories\Tags;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Routing\Controller;
@@ -17,11 +18,21 @@ class Tag extends Controller
 		return view("tag.index")->with(compact('table'));
 	}
 
-	public function view(Guard $auth, Tags $tags, $tagname)
+	public function view(Guard $auth, Posts $posts, Tags $tags, $tagname)
 	{
 		$tag = $tags->findOneBy(["tag" => $tagname]);
 
-		return view('tag.view')->with(compact('tag'));
+		$query = $posts->createQueryBuilder("p")
+			->leftJoin("p.discussions", "d")
+			->leftJoin("d.tag", "t")
+			->select("p", "d", "t")
+			->where("t = :tag")
+			->setParameter(":tag", $tag)
+			->orderBy('p.recentActivity', "DESC");
+
+		$table = $tags->paginate($query->getQuery(), 20);
+
+		return view('tag.view')->with(compact('table'));
 	}
 
 }
--- Hash/resources/views/tag/view.blade.php
+++ Hash/resources/views/tag/view.blade.php
@@ -1,11 +1,36 @@
 @php
-	/** @var $tag App\Entities\Tag */
+	/** @var Illuminate\Pagination\LengthAwarePaginator|App\Entities\Post[] $table */
 	/** @var $form Collective\Html\FormBuilder */
 @endphp
 
 @inject('form', 'Collective\Html\FormBuilder')
 @extends('theme.base')
-@section('title', $tag->getTag())
+@section('title', 'hi')
 @section('content')
+
+	@foreach($table as $post)
+		<h2>
+			<a href="/post/{{$post->getId()}}">{{ $post->getTitle() }}</a>
+			<small class="text-muted">By: <a href="{{ action('User@view', ['username' => $post->getAuthor()->getUsername()]) }}">{{ $post->getAuthor()->getUsername() }}</a></small>
+		</h2>
+
+		@if($post->getLink())
+			<h4><a href="{{ $post->getLink() }}">{{ $post->getLink() }}</a></h4>
+		@else
+			<em>Text Post</em>
+		@endif
+
+		@markdown($post->getBody())
+
+		<div class="btn-toolbar">
+			@foreach($post->getDiscussions() as $d)
+				@component("ajax.tag")
+					@slot("discussion", $d)
+				@endcomponent
+			@endforeach
+		</div>
+
+		<hr>
+	@endforeach
 
 @endsection
